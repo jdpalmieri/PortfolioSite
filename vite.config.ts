209591213aeaba1869ec1a0bfbbcb1a24149c527
jdpalmieri <jondpalm@gmail.com>
2026-03-09 08:39:36 -0700

    Live Test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PortfolioSite/',  // replace with your actual repo name
 })
